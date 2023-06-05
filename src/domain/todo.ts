c7fd5c015a51bab1e44668dbffe9908d2eb4fa83
otumian-empire <popecan1000@gmail.com>
2023-06-05 02:01:22 +0000

    implement the domain

---
+++ src/domain/todo.ts
@@ -0,0 +1,9 @@
+export default class Todo {
+    id: number;
+    title: string;
+    body: string;
+    isDone: boolean;
+    userId: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ src/domain/user.ts
@@ -0,0 +1,10 @@
+export default class User {
+    id: number;
+    email: string;
+    username: string;
+    password: string;
+    isVerified: boolean;
+    isActive: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}
